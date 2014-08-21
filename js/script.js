bacb1b0b846a2f5dff604ca08dcc8fcd66274f42
hanif <hanif@megusta.ag>
2014-08-22 01:49:02 +0600

    Updated

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-	$('.search-keyword').focus(function(){
-		$(this).closest('.keywrod-place').addClass('active');
-	});
-	$('.search-keyword').blur(function(){
-		$(this).closest('.keywrod-place').removeClass('active');
-	});
-});
